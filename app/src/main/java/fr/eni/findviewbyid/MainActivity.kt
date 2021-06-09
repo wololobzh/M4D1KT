ee43c1b9ecba732733a888cf620a107239dd44f2
cosson <acosson@eni-ecole.fr>
2021-06-09 15:27:22 +0200

    Initial commit

---
+++ app/src/main/java/fr/eni/findviewbyid/MainActivity.kt
@@ -0,0 +1,22 @@
+package fr.eni.findviewbyid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.RatingBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.tv_titre)
+        val editText = findViewById<EditText>(R.id.et_info_edit_text)
+        val ratingBar = findViewById<RatingBar>(R.id.rb_une_note)
+
+        textView.text = "Hello";
+        editText.setText("Comment allez vous ?")
+        ratingBar.rating = 3F;
+    }
+}
